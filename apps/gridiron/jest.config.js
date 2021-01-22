d73f92aaea7880f9d9de55dcece461b32dd54d24
michael speed <michael.the.don@gmail.com>
2021-01-23 02:26:07 +0530

    initial commit

---
+++ apps/gridiron/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'gridiron',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/gridiron',
+};
